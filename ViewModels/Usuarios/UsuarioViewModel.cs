e4cdd9e92439c38f1fb1f40702628cab241b6bf0
renan <renanalmeidadantas2007@gmail.com>
2025-03-11 22:35:33 -0300

    inicio da programação em mvvm da nossa api em c#, na materia de PAMII

---
+++ ViewModels/Usuarios/UsuarioViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppRpgEtec.ViewModels.Usuarios
+{
+    internal class UsuarioViewModel
+    {
+    }
+}
---
+++ Views/Usuarios/LoginView.xaml.cs
@@ -0,0 +1,9 @@
+namespace AppRpgEtec.Views.Usuarios;
+
+public partial class NewPage1 : ContentPage
+{
+	public NewPage1()
+	{
+		InitializeComponent();
+	}
+}
